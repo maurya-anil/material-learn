02b2e0a12d38fff55a780c41082b556f4f3e39c3
Anil Maurya <maurya.anil@gmail.com>
2023-02-06 21:25:23 +0530

    dashboard component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ClientListComponent} from '../app/client-list/client-list.component';
+import {DashboardComponent} from '../app/dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path:'', component:ClientListComponent}
+  {path:'clientlist', component:ClientListComponent},
+  {path:'', component:DashboardComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,12 +15,17 @@ import { ClientListComponent } from './client-list/client-list.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent,
-    ClientListComponent
+    ClientListComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
@@ -34,7 +39,10 @@ import { MatSortModule } from '@angular/material/sort';
     MatListModule,
     MatTableModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
